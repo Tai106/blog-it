4ccfe88042346ba6ce0afe246125ab37a8787e44
Gravity <tevoned@gmail.com>
2023-07-17 20:44:21 -0400

    login page

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 auth = Blueprint("auth", __name__)
 
 @auth.route("/login")
 def login():
-    return "Login"
+    return render_template("login.html")
 
 @auth.route("/sign-up")
 def sign_up():
-    return "Sign Up"
+    return render_template("signup.html")
 
 
 @auth.route("/logout")
 def logout():
-    return "Log Out"
+    return redirect(url_for("views.home"))
