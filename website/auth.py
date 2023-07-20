ae86ac8877abb300d682180f8c15658d4e7a0cd5
Gravity <tevoned@gmail.com>
2023-07-19 21:24:22 -0400

    login functionality

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,29 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import db
 from .models import User
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint("auth", __name__)
 
 
-
-
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
-    email = request.form.get("email")
-    password = request.form.get("password")
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in!", category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Password is incorrect.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
     return render_template("login.html")
 
 
@@ -37,12 +48,14 @@ def sign_up():
             flash('Username is too short.', category='error')
         elif len(password1) < 5:
             flash('Password is too short.', category='error')
-        elif len(email) <  4:
+        elif len(email) < 4:
             flash("Email is invalid.", category='error')
         else:
-            new_user = User(email=email, username=username, password=generate_password_hash(password1, method='sha256'))
+            new_user = User(email=email, username=username, password=generate_password_hash(
+                password1, method='sha256'))
             db.seesion.add(new_user)
             db.session.commit()
+            login_user(new_user, remember=True)
             flash('User created!')
             return redirect(url_for('views.home'))
 
@@ -50,5 +63,7 @@ def sign_up():
 
 
 @auth.route("/logout")
+@login_required
 def logout():
+    logout_user()
     return redirect(url_for("views.home"))
